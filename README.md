8bb0c0dac6e4d93b03bbcea957a656c11c4243bb
Gunter Grodotzki <gunter@grodotzki.co.za>
2017-11-27 07:47:49 +0200

    init repo (#1)
    --

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# ansible-role-glassfish
-Ansible role that installs GlassFish on Debian-like systems
+[![Build Status](https://travis-ci.org/NINEJKH/ansible-role-glassfish.svg?branch=master)](https://travis-ci.org/NINEJKH/ansible-role-glassfish)
+
+# NINEJKH.glassfish
+
+An Ansible role that installs glassfish on Debian-like systems.
+
+## Requirements
+
+none
+
+## Role Variables
+
+```yaml
+glassfish_version: 3.1.2.2
+```
+
+## Dependencies
+
+* [lifeofguenter.oracle-java](https://galaxy.ansible.com/lifeofguenter/oracle-java/)
+
+## Example Playbook
+
+```yaml
+
+- hosts: glassfishes
+  roles:
+    - { role: NINEJKH.glassfish }
+```
+
+## License
+
+Licensed under the MIT License. See the [LICENSE file](LICENSE) for details.
+
+## Author Information
+
+[9JKH (Pty) Ltd.](https://9jkh.co.za)
---
+++ test.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/env bash
+
+__DIR__="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# required libs
+source "${__DIR__}/.bash/functions.shlib"
+
+set -E
+trap 'throw_exception' ERR
+
+usage() {
+cat <<EOF
+Usage: ${0##*/} OPTIONS
+test role
+
+    -h          display this help and exit
+    -i STRING   target host to run against (default: 127.0.0.1)
+    -u STRING   connect as this user (default: current user)
+EOF
+}
+
+OPTIND=1
+
+while getopts "hi:u:" opt; do
+  case "${opt}" in
+    h )
+      usage
+      exit 0
+      ;;
+    i )
+      TARGET_HOST="${OPTARG}"
+      ;;
+    u )
+      CONNECT_USER="${OPTARG}"
+      ;;
+    '?' )
+      usage >&2
+      exit 1
+      ;;
+  esac
+done
+shift "$((OPTIND-1))"
+
+if [[ -z "${TARGET_HOST}" ]]; then
+  TARGET_HOST="127.0.0.1"
+  CONNECTION="local"
+fi
+
+if [[ -z "${CONNECT_USER}" ]]; then
+  CONNECT_USER="$(whoami)"
+fi
+
+if [[ -z "${CONNECTION}" ]]; then
+  CONNECTION="smart"
+fi
+
+role_root="$(pwd)"
+
+consolelog "installing requirements"
+ansible-galaxy install lifeofguenter.oracle-java
+
+consolelog "running role as playbook #1"
+ansible-playbook \
+  --inventory="${TARGET_HOST}," \
+  --user="${CONNECT_USER}" \
+  --extra-vars="role_root=${role_root}" \
+  --connection="${CONNECTION}" \
+  tests/test.yml
+
+consolelog "running role as playbook #2"
+ansible-playbook \
+  --inventory="${TARGET_HOST}," \
+  --user="${CONNECT_USER}" \
+  --extra-vars="role_root=${role_root}" \
+  --connection="${CONNECTION}" \
+  tests/test.yml
+
+# waitfor 5min
+while [[ "${i}" -lt "60" ]]; do
+  i="$((i + 1))"
+  if ! curl \
+    --head \
+    --silent \
+    --fail \
+    --show-error \
+    "${TARGET_HOST}:8080" 2> /dev/null; then
+    consolelog "waitfor ${TARGET_HOST}:8080 #${i}"
+    sleep 5
+  else
+    success="1"
+    break
+  fi
+done
+
+if [[ -z "${success}" ]]; then
+  consolelog "wildfly unreachable" "error"
+  exit 1
+fi
